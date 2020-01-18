df4bbcb51641d019f24f5c3dba4eccd3cbc62e21
bionade24 <o.roesler@oscloud.info>
2020-01-18 09:52:35 +0100

    Update udev commands in scripts/

--- scripts/delete_udev_rules.sh
+++ scripts/delete_udev_rules.sh
@@ -2,10 +2,10 @@
 
 echo "delete remap the device serial port(ttyUSBX) to  rplidar"
 echo "sudo rm   /etc/udev/rules.d/rplidar.rules"
-sudo rm   /etc/udev/rules.d/rplidar.rules
+sudo rm   /etc/udev/rules.d/*rplidar.rules
 echo " "
 echo "Restarting udev"
 echo ""
-sudo service udev reload
-sudo service udev restart
+sudo udevadm control --reload
+sudo udevadm trigger
 echo "finish  delete"
